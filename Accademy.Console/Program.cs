c40bec126b69b8130b91deb64799e4a69d6ffb09
angela.colella <angela.colella@WINAPIUZIYVIF6L>
2020-11-03 14:42:28 +0100

    first commit Accademy

--- Accademy.Console/Program.cs
+++ Accademy.Console/Program.cs
@@ -16,6 +16,10 @@ namespace Accademy.Console
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //TestGeometry();
